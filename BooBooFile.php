69b2fa3f838faf1faed12c814191c904f08072a6
KristiJov <kristina.d.jovanovic@gmail.com>
2015-05-26 12:59:48 +0200

    Create BooBooFile.php

---
+++ BooBooFile.php
@@ -0,0 +1,7 @@
+<?php>
+  echo "This is our group!!!!!! :) :)";
+  echo "This is me writing my name and student number";
+  echo "Kristina Jovanovic - 12214982";
+  echo "This is the other group member writing her name and student number, but from her side.";
+  echo "WE ARE COLLABORATING"; 
+</php>
