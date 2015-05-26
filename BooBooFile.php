eacc0253fd36f6a1b3d45e124db77e4d89a47713
Gezile <gezile.ndhlovu@hotmail.com>
2015-05-26 13:07:10 +0200

    Update BooBooFile.php

--- BooBooFile.php
+++ BooBooFile.php
@@ -1,7 +1,12 @@
-<?php>
+<?php
+  echo "HELLO WORLD! (Rendani and Theo ;) )";
   echo "This is our group!!!!!! :) :)";
   echo "This is me writing my name and student number";
   echo "Kristina Jovanovic - 12214982";
   echo "This is the other group member writing her name and student number, but from her side.";
-  echo "WE ARE COLLABORATING"; 
-</php>
+  echo "WE ARE COLLABORATING";  
+  
+  echo "This is the other boo boo writing her name and student number"; 
+  echo "Gezile Ndhlovu -12240282"; 
+  echo "WE HAVE NOW FINISHED COLLABORATING :)"
+?>
